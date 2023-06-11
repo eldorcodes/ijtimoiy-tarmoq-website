92f3ce7a8b1deb6f1b8fac04c5e89bbe517a072c
Eldor Togaymurotov <eldorcodes@gmail.com>
2023-06-10 21:05:52 -0400

    donee

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 4000;
 const path = require('path');
 app.use(express.static('build'));
-app.get('/',(req,res) => {
+app.get('*',(req,res) => {
     res.sendFile(path.resolve(__dirname,'build','index.html'))
 });
 app.listen(port,(err) => {
